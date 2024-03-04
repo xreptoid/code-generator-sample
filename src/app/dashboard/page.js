1703c23c36fa917548b132ddc858037a71732adc
Ilya Korovin <ilya.krvn@gmail.com>
2024-03-04 21:56:55 +0400

    shadcn ui; import repos form

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,19 @@
 import Reptoid from "@reptoid/codebases"
 import { redirect } from "next/navigation"
+import { cn } from "@/lib/utils"
+import ImportRepoForm from "./components/ImportRepoForm"
+import { Button } from "@/components/ui/button"
 
 export const dynamic = 'force-dynamic'
 
-const reptoid = new Reptoid({ accessToken: process.env.REPTOID_ACCESS_TOKEN })
+const reptoid = new Reptoid({
+    apiHost: 'http://localhost:3010',
+    accessToken: process.env.REPTOID_ACCESS_TOKEN
+})
 
 export default async function Page() {
     const accountId = '230145c4-69c6-4f49-a4c2-0d1c6ba993a3'
+    const repos = await reptoid.account(accountId).github().repos()
     const workspaces = await reptoid.account(accountId).workspaces().all()
 
     async function create(formData) {
@@ -16,10 +23,14 @@ export default async function Page() {
     }
 
     return <div className="p-20">
-        <div className="text-xl font-bold">Workspaces</div>
+        <div className={cn("text-xl font-bold")}>Import GitHub repository</div>
+        <div className="mt-4">
+            <ImportRepoForm repos={repos.map(repo => ({ label: repo, value: repo }))}/>
+        </div>
+        <div className="text-xl font-bold mt-10">Workspaces</div>
         <div className="mt-4">
             <form action={create}>
-                <button className="bg-black font-bold text-white px-3 py-2">Create New</button>
+                <Button>Create New</Button>
             </form>
         </div>
         <div className="mt-6">
